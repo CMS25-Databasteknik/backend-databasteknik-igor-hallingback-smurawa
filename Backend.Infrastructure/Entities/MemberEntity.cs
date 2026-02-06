36856f099eb7832737cab099d2237791ae92b0af
ihsnack <ihsnack@fastmail.com>
2026-02-06 14:13:48 +0100

    kodbaserad modell för SQL-databas med Entity Framework Core 21:36

---
+++ Backend.Infrastructure/Entities/MemberEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Infrastructure.Entities
+{
+    public class MemberEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Column(TypeName = "VARCHAR(20)")]
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
+        public byte[] Concurrency { get; set; } = null!;
+
+        [Column(TypeName = "datetime2(0)")]
+        public DateTime CreatedAtUtc { get; set; }
+        public DateTime ModifiedAtUtc { get; set; }
+    }
+}
