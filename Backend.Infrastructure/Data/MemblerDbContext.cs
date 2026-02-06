266f126b9f3ea642f469300182163df91cf07702
ihsnack <ihsnack@fastmail.com>
2026-02-06 14:38:53 +0100

    kodbaserad modell för SQL-databas med Entity Framework Core 40:41

--- Backend.Infrastructure/Data/MemblerDbContext.cs
+++ Backend.Infrastructure/Data/MemblerDbContext.cs
@@ -17,6 +17,43 @@ namespace Backend.Infrastructure.Data
                 entity.Property(entity => entity.Id)
                     .ValueGeneratedOnAdd()
                     .HasDefaultValueSql("(NEWSEQUENTIALID())", "DF_Members_Id");
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(256)
+                    .IsRequired();
+
+                entity.Property(e => e.PhoneNumber)
+                    .HasMaxLength(123)
+                    .IsUnicode(false)
+                    .IsRequired(false);
+
+                entity.Property(e => e.Concurrency)
+                    .IsRowVersion()
+                    .IsConcurrencyToken()
+                    .IsRequired();
+
+                entity.Property(e => e.CreatedAtUtc)
+                    .HasPrecision(0) // datetime2(0) 14:35:15.0123
+                    .HasDefaultValueSql("(SYSUTCDATETIME())", "DF_Members_CreatedAtUtc")
+                    .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.ModifiedAtUtc)
+                    .HasPrecision(0) // datetime2(0) 14:35:15.0123
+                    .HasDefaultValueSql("(SYSUTCDATETIME())", "DF_Members_ModifiedAtUtc")
+                    .ValueGeneratedOnAddOrUpdate();
+
+                entity.HasIndex(e => e.Email, "UQ_Members_Email").IsUnique();
+
+                entity.ToTable(tb => tb.HasCheckConstraint("CK_Members_Email_NotEmpty", "LTRIM(RTRIM([Email])) <> ''"));
+
             });
 
             base.OnModelCreating(modelBuilder);
