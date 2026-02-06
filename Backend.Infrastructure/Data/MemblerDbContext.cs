d74f65370a3bb66386f4a8fce91973040e18544c
ihsnack <ihsnack@fastmail.com>
2026-02-06 14:25:22 +0100

    kodbaserad modell för SQL-databas med Entity Framework Core 30:42

--- Backend.Infrastructure/Data/MemblerDbContext.cs
+++ Backend.Infrastructure/Data/MemblerDbContext.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Backend.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Infrastructure.Data
 {
     public sealed class MemblerDbContext(DbContextOptions<MemblerDbContext> options) : DbContext(options)
     {
+        public DbSet<MemberEntity> Members => Set<MemberEntity>();
         override protected void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<MemberEntity>(entity =>
+            {
+                entity.ToTable("Members");
+
+                entity.HasKey(e => e.Id).HasName("PK_Members_Id");
+
+                entity.Property(entity => entity.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasDefaultValueSql("(NEWSEQUENTIALID())", "DF_Members_Id");
+            });
+
             base.OnModelCreating(modelBuilder);
         }
     }
