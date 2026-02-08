dcd6d84f7aa7ae0814b8463ae259d5c4db323e25
ihsnack <ihsnack@fastmail.com>
2026-02-08 17:40:13 +0100

    feat: add dependencies and start coursesOnlineDbContext

---
+++ Backend.Infrastructure/Data/CoursesOnlineDbContext.cs
@@ -0,0 +1,59 @@
+﻿using Backend.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Infrastructure.Data
+{
+    public sealed class CoursesOnlineDbContext(DbContextOptions<CoursesOnlineDbContext> options) : DbContext(options)
+    {
+        public DbSet<CourseEntity> Courses => Set<CourseEntity>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CourseEntity>(entity =>
+            {
+                entity.ToTable("Courses", tb =>
+                    tb.HasCheckConstraint("CK_Courses_Title_NotEmpty", "LTRIM(RTRIM([Title])) <> ''"));
+
+                entity.HasKey(e => e.Id).HasName("PK_Courses_Id");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasDefaultValueSql("(NEWSEQUENTIALID())", "DF_Courses_Id");
+
+                entity.Property(e => e.Title)
+                    .HasMaxLength(100)
+                    .IsRequired();
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(500)
+                    .IsRequired();
+
+                entity.Property(e => e.Length)
+                    .IsRequired();
+
+                entity.Property(e => e.Concurrency)
+                    .IsRowVersion()
+                    .IsConcurrencyToken()
+                    .IsRequired();
+
+                entity.Property(e => e.CreatedAtUtc)
+                    .HasPrecision(0)
+                    .HasDefaultValueSql("(SYSUTCDATETIME())", "DF_Courses_CreatedAtUtc")
+                    .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.ModifiedAtUtc)
+                    .HasPrecision(0)
+                    .HasDefaultValueSql("(SYSUTCDATETIME())", "DF_Courses_ModifiedAtUtc")
+                    .ValueGeneratedOnAddOrUpdate();
+
+                entity.HasIndex(e => e.Title)
+                    .HasDatabaseName("IX_Courses_Title");
+
+                entity.HasMany(c => c.CourseEvents)
+                    .WithOne(ce => ce.Course)
+                    .HasForeignKey(ce => ce.CourseId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
+    }
+}
--- Backend.Infrastructure/Entities/CourseEntity.cs
+++ Backend.Infrastructure/Entities/CourseEntity.cs
@@ -6,6 +6,9 @@
         public string Title { get; set; } = null!;
         public string Description { get; set; } = null!;
         public int Length { get; set; }
+        public byte[] Concurrency { get; set; } = null!;
+        public DateTime CreatedAtUtc { get; set; }
+        public DateTime ModifiedAtUtc { get; set; }
         public virtual ICollection<CourseEventEntity> CourseEvents { get; set; } = [];
     }
 }
