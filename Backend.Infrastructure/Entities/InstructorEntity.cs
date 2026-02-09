77c00ee9f4db5d01d7c3b2a2416fbacbac46186d
Igor Hallingbäck Smurawa <ihsnack@fastmail.com>
2026-02-09 09:22:56 +0100

    Feature/initial database tables (#3)
    * feat: add course related entities
    
    * feat: add dependencies and start coursesOnlineDbContext
    
    * feat: update & cleanup CoursesOnlineDbContext, update entities, prepare for migration
    
    * feat: add migration for initial tables

---
+++ Backend.Infrastructure/Entities/InstructorEntity.cs
@@ -0,0 +1,9 @@
+namespace Backend.Infrastructure.Entities
+{
+    public class InstructorEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public virtual ICollection<CourseEventEntity> CourseEvents { get; set; } = [];
+    }
+}
