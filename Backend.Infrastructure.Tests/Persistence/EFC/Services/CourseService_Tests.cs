62682ba5bc97cf6d7d05a8e284a67df33db63ec4
ihsnack <ihsnack@fastmail.com>
2026-02-13 16:17:35 +0100

    fix: testing

---
+++ Backend.Infrastructure.Tests/Persistence/EFC/Services/CourseService_Tests.cs
@@ -0,0 +1,25 @@
+﻿using Backend.Application.Modules.Courses;
+using Backend.Application.Modules.Courses.Inputs;
+using Backend.Domain.Modules.Courses.Contracts;
+using Backend.Domain.Modules.Courses.Models;
+using NSubstitute;
+
+namespace Backend.Infrastructure.Tests.Persistence.EFC.Services;
+
+public class CourseService_Tests
+{
+    [Fact]
+    public async Task CreateCourseAsync_Should_Create_Course()
+    {
+        // Arrange
+        var repo = Substitute.For<ICoursesRepository>();
+        await repo.CreateCourseAsync(Arg.Any<Course>(), Arg.Any<CancellationToken>());
+
+        // Act
+        var service = new CourseService(repo);
+        var input = new CreateCourseInput("Test Course", "Test Description", 5);
+
+        // Assert
+
+    }
+}
