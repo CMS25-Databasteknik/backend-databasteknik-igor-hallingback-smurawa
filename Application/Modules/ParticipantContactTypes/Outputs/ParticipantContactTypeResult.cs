c5e1107ac60a5a9738cd927551ab601dbb22a1c3
ihsnack <ihsnack@fastmail.com>
2026-02-27 10:54:32 +0100

    fix: cleanup

--- Application/Modules/ParticipantContactTypes/Outputs/ParticipantContactTypeResult.cs
+++ Application/Modules/ParticipantContactTypes/Outputs/ParticipantContactTypeResult.cs
@@ -5,18 +5,12 @@ namespace Backend.Application.Modules.ParticipantContactTypes.Outputs;
 
 public sealed record ParticipantContactTypeResult : ResultBase<ParticipantContactType>
 {
-    [System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
-    public ParticipantContactTypeResult() : base() { }
 }
 
 public sealed record ParticipantContactTypeListResult : ResultBase<IReadOnlyList<ParticipantContactType>>
 {
-    [System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
-    public ParticipantContactTypeListResult() : base() { }
 }
 
 public sealed record ParticipantContactTypeDeleteResult : ResultBase<bool>
 {
-    [System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
-    public ParticipantContactTypeDeleteResult() : base() { }
 }
